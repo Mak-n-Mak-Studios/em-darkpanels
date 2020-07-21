ed1bc92f99e9eb3cc202838de3cd505dc3ad1dd5
TheAsian EggrollMaker <BLEEPBLOOPforLife@users.noreply.github.com>
2020-07-20 17:48:16 -0700

    Added a copyright notice to em_darkpanels_load.lua
    Added a copyright notice to em_darkpanels_load.lua.

--- lua/autorun/em_darkpanels_load.lua
+++ lua/autorun/em_darkpanels_load.lua
@@ -1,3 +1,7 @@
+--[[---------------------------------------------------------------------------
+Copyright (c) 2020 TheAsian EggrollMaker & MelonShooter. All rights reserved.
+--]]---------------------------------------------------------------------------
+
 -- Automatically load the necessary components of em_darkpanels. Parts not packaged in this addon will not be included.
 
 local dark_panels_content_found = file.Find( "em_darkpanels/*.lua", "LUA" )
